cd95e8647b9c551c35d67895825ea84c86180410
Trekhub <Teddyomondi001@gmail.com>
2024-05-21 11:12:38 +0300

    chore: Add lucide-react dependency and update font family

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import { HomePage } from "./pages/home/HomePage";
+import { NavBar } from "./components/NavBar";
 
 function App() {
   return (
-    <div>
+    <div className="font-urbanist">
       <Router>
+        <NavBar />
         <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
-          <Route path="/auth" element={<h1>About</h1>} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/auth" element={<h1>Auth</h1>} />
+          <Route path="/movies" element={<h1>Movies</h1>} />
+          <Route path="/tv-shows" element={<h1>Tv Shows</h1>} />
         </Routes>
       </Router>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "node_modules/flowbite-react/lib/esm/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        urbanist: ["Urbanist", "sans-serif"],
+      },
+    },
   },
   plugins: [flowbitePlugin],
 };
